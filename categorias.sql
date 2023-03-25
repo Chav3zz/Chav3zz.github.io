83a7188b2f414d9fab9577949ff4be1001a69b9b
Chav3zz <124410868+Chav3zz@users.noreply.github.com>
2023-03-24 19:45:45 -0600

    Add files via upload

--- categorias.sql
+++ categorias.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 28, 2023 at 01:43 AM
--- Server version: 10.1.39-MariaDB
--- PHP Version: 7.3.5
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-03-2023 a las 02:44:28
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tienda_online`
+-- Base de datos: `categorias`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categorias`
+-- Estructura de tabla para la tabla `categorias`
 --
 
 CREATE TABLE `categorias` (
@@ -35,35 +35,71 @@ CREATE TABLE `categorias` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `categorias`
+-- Volcado de datos para la tabla `categorias`
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `descripcion`) VALUES
-(1, 'Nuevos', 'Se trata de aquellos productos que se ubican en etapa de lanzamiento y que esperan la respuesta del consumidor sobre sus características.'),
-(2, 'Estrella', 'Los productos estrella resaltan por el boom que tienen en ventas al momento de su lanzamiento, hasta que sale un producto superior. Sin embargo, también están aquellos que se mantienen vigentes durante años como la estrella en su segmento.'),
-(3, 'Temporada', 'Los disfraces para halloween o las rosas para el 14 de febrero son ejemplos de este tipo de producto que se vende sólo durante un intervalo específico.'),
-(4, 'Seguidores', 'Estos productos también destacan en su segmento, pero siempre detrás de los estrella, por ejemplo las tabletas electrónicas y los drones.'),
-(5, 'A la baja', 'No consideraron las necesidades del consumidor sino que surgieron por moda o son productos con versiones que se actualizan cada año; por ejemplo los Smartphones, que se renuevan constantemente.');
+(1, 'Electrónicos', '\r\nAccesorios y suministros\r\nCámaras y Fotografía\r\nElectrónicos para autos y vehículos\r\nTeléfonos celulares y accesorios\r\nEquipos y accesorios\r\nGPS y navegación\r\nAudífonos\r\nAudio para el hogar\r\nElectró'),
+(2, 'Computadoras', '\r\nAccesorios y periféricos para computadoras\r\nComponentes de computadoras\r\nComputadoras y tablets\r\nAlmacenamiento de datos\r\nComponentes externos\r\nAccesorios para computadoras portátiles\r\nMonitores\r\nPr'),
+(3, 'Smart Home', '\r\nAmazon Smart Home\r\nIluminación de Smart Home\r\nCerraduras Inteligentes y Entrada\r\nCámaras y Sistemas de Seguridad\r\nEnchufes\r\nNuevos Dispositivos Inteligentes\r\nCalefacción y Refrigeración\r\nDetectores '),
+(4, 'Arte Y Artesanías', '\r\nSuministros para pintura, dibujo y arte\r\nCuentas y joyería\r\nArtesanías\r\nTela\r\nDecoración de tela\r\nTejido y crochet\r\nBordado\r\nOrganización, almacenamiento y transporte\r\nEstampado\r\nColección de recort'),
+(5, 'Automotriz', '\r\nCuidado de automóviles\r\nElectrónicos y accesorios para autos\r\nAccesorios exteriores\r\nAccesorios interiores\r\nLuces y accesorios de iluminación\r\nMotociclismo y deportes de motor\r\nAceites y fluidos\r\nPi');
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `precio` float NOT NULL,
+  `categoria_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `categorias`
+-- Indices de la tabla `categorias`
 --
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_id` (`categoria_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `categorias`
+-- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
