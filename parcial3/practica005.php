d7be25eb0a02c9df468f537a6d4683a61d898f1a
Chav3zz <124410868+Chav3zz@users.noreply.github.com>
2023-06-06 17:51:56 -0700

    Práctica 5 06/06/2023

--- parcial3/practica005.php
+++ parcial3/practica005.php
@@ -17,83 +17,85 @@
         }
     }
 
+
+
     if($cuadros[0] == "X" && $cuadros[1] == "X" && $cuadros[2] == "X"){
         echo "<h3>Ganaron las X</h3><br>";
-        echo "Cuadros ganadores: 1 , 2 y 3.";
+        echo "<h2>[X][X][X]<br>[ ][ ][ ]<br>[ ][ ][ ]</h2>";
         return;
     }else if($cuadros[0] == "O" && $cuadros[1] == "O" && $cuadros[2] == "O"){
         echo "<h3>Ganaron las O</h3><br>";
-        echo "Cuadros ganadores: 1 , 2 y 3.";
+        echo "<h2>[O][O][O]<br>[ ][ ][ ]<br>[ ][ ][ ]</h2>";
         return;
     }
 
     if($cuadros[3] == "X" && $cuadros[4] == "X" && $cuadros[5] == "X"){
         echo "<h3>Ganaron las X</h3><br>";
-        echo "Cuadros ganadores: 4, 5 y 6.";
+        echo "<h2>[ ][ ][ ]<br>[X][X][X]<br>[ ][ ][ ]</h2>";
         return;
     }else if($cuadros[3] == "O" && $cuadros[4] == "O" && $cuadros[5] == "O"){
         echo "<h3>Ganaron las O</h3><br>";
-        echo "Cuadros ganadores: 4, 5 y 6.";
+        echo "<h2>[ ][ ][ ]<br>[O][O][O]<br>[ ][ ][ ]</h2>";
         return;
     }
 
     if($cuadros[6] == "X" && $cuadros[7] == "X" && $cuadros[8] == "X"){
         echo "<h3>Ganaron las X</h3><br>";
-        echo "Cuadros ganadores: 7, 8 y 9.";
+        echo "<h2>[ ][ ][ ]<br>[ ][ ][ ]<br>[X][X][X]</h2>";
         return;
     }else if($cuadros[6] == "O" && $cuadros[7] == "O" && $cuadros[8] == "O"){
         echo "<h3>Ganaron las O</h3><br>";
-        echo "Cuadros ganadores: 7, 8 y 9.";
+        echo "<h2>[ ][ ][ ]<br>[ ][ ][ ]<br>[O][O][O]</h2>";
         return;
     }
 
     if($cuadros[0] == "X" && $cuadros[3] == "X" && $cuadros[6] == "X"){
         echo "<h3>Ganaron las X</h3><br>";
-        echo "Cuadros ganadores: 1, 4 y 7.";
+        echo "<h2>[X][ ][ ]<br>[X][ ][ ]<br>[X][ ][ ]</h2>";
         return;
     }else if($cuadros[0] == "O" && $cuadros[3] == "O" && $cuadros[6] == "O"){
         echo "<h3>Ganaron las O</h3><br>";
-        echo "Cuadros ganadores: 1, 4 y 7.";
+        echo "<h2>[O][ ][ ]<br>[O][ ][ ]<br>[O][ ][ ]</h2>";
         return;
     }
 
     if($cuadros[1] == "X" && $cuadros[4] == "X" && $cuadros[7] == "X"){
         echo "<h3>Ganaron las X</h3><br>";
-        echo "Cuadros ganadores: 2, 5 y 8.";
+        echo "<h2>[ ][X][ ]<br>[ ][X][ ]<br>[ ][X][ ]</h2>";
         return;
     }else if($cuadros[1] == "O" && $cuadros[4] == "O" && $cuadros[7] == "O"){
         echo "<h3>Ganaron las O</h3><br>";
-        echo "Cuadros ganadores: 2, 5 y 8.";
+        echo "<h2>[ ][O][ ]<br>[ ][O][ ]<br>[ ][O][ ]</h2>";
         return;
     }
 
     if($cuadros[2] == "X" && $cuadros[5] == "X" && $cuadros[8] == "X"){
         echo "<h3>Ganaron las X</h3><br>";
-        echo "Cuadros ganadores: 3, 6 y 9.";
+        echo "<h2>[ ][ ][X]<br>[ ][ ][X]<br>[ ][ ][X]</h2>";
         return;
     }else if($cuadros[2] == "O" && $cuadros[5] == "O" && $cuadros[8] == "O"){
         echo "<h3>Ganaron las O</h3><br>";
-        echo "Cuadros ganadores: 3, 6 y 9.";
+        echo "<h2>[ ][ ][O]<br>[ ][ ][O]<br>[ ][ ][O]</h2>";
         return;
     }
 
     if($cuadros[0] == "X" && $cuadros[4] == "X" && $cuadros[8] == "X"){
         echo "<h3>Ganaron las X</h3><br>";
-        echo "Cuadros ganadores: 1, 5 y 9.";
+        echo "<h2>[X][ ][ ]<br>[ ][X][ ]<br>[ ][ ][X]</h2>";
         return;
     }else if($cuadros[0] == "O" && $cuadros[4] == "O" && $cuadros[8] == "O"){
         echo "<h3>Ganaron las O</h3><br>";
-        echo "Cuadros ganadores: 1, 5 y 9.";
+        echo "<h2>[O][ ][ ]<br>[ ][O][ ]<br>[ ][ ][O]</h2>";
         return;
     }
 
     if($cuadros[2] == "X" && $cuadros[4] == "X" && $cuadros[6] == "X"){
         echo "<h3>Ganaron las X</h3><br>";
-        echo "Cuadros ganadores: 3, 5 y 7.";
+        echo "<h2>[ ][ ][X]<br>[ ][X][ ]<br>[X][ ][ ]</h2>";
         return;
     }else if($cuadros[2] == "O" && $cuadros[4] == "O" && $cuadros[6] == "O"){
         echo "<h3>Ganaron las O</h3><br>";
-        echo "Cuadros ganadores: 3, 5 y 7.";
+        echo "<h2>[ ][ ][O]<br>[ ][O][ ]<br>[O][ ][ ]</h2>";
         return;
     }
 
